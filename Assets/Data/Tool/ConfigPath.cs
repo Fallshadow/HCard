e26131583bc09ed0ea40a63acd618c26babd74de
sunshuchao <www.sunshuchao@outlook.com>
2020-02-23 11:38:44 +0800

    ui，读表，事件，单例，debuglog

---
+++ Assets/Data/Tool/ConfigPath.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace act.data
+{
+    public enum CONFIG_PATH
+    {
+        //範例
+        [Description("dict/dict_confingTest")] DICT_EXAMPLE,
+        [Description("dict/dict_test")] DICT_TEST,
+
+    }
+}
